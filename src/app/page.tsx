12df5538afd4368bd3ffae3c07345ccd3e38d07d
Aliyu Nuhu <serviceguru@svoltnigeria.com>
2025-11-29 22:12:18 +0000

    the footer should be aligned to the center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,10 +144,10 @@ export default function Home() {
       </main>
 
       <footer className="border-t">
-        <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-          <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+        <div className="container flex h-24 items-center justify-center py-10">
+          <div className="flex flex-col items-center gap-2 md:flex-row">
             <Logo imageUrl={logoUrl} className="h-6 w-6 text-primary" />
-            <p className="text-center text-sm leading-loose md:text-left">
+            <p className="text-center text-sm leading-loose">
               Built by Service Guru. &copy; {new Date().getFullYear()} All rights reserved.
             </p>
           </div>
