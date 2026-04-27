dabafa7a9523c9adf0ff03ed97964ffb799c0cc7
Aliyu <serviceguru@svoltnigeria.com>
2026-04-27 14:13:15 +0100

    chore: add fallback default values to firebase configuration for development environment

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 // It reads the configuration from your .env file.
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDYitdmm5H_7GkRR8PAmPMIyAQ9sS9Xggc",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-1298078893-e7941.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-1298078893-e7941",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-1298078893-e7941.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1018583979606",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1018583979606:web:d50ec535e8fbc4c231cdf7",
 };
